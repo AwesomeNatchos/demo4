0a7e0f6d29239845d621c2bb83085c9d357353e1
Natcha Uthaug <natcha.uthaug@gmail.com>
2023-02-12 23:23:17 +0100

    created new login form

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,42 @@
-  </body>
+<!--
+<nav class="navbar navbar-light bg-light">
+  <div class="text-center">
+    <p3>Created by</p3>
+  </div>
+  
+</nav>
+-->
 
-  </html>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <!-- Container wrapper -->
+  <div class="container-fluid">
+    <!-- Collapsible wrapper -->
+    <div class="navbar-collapse justify-content-center" >
+      <!-- Left links -->
+      <ul class="navbar-nav mb-2 mb-lg-0">
+        <li class="nav-item">
+        <a class="nav-link "  href="#">Created by</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link " href="#">Chaimae</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Dita</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Ibrahim</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Natcha</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Ussaid</a>
+        </li>
+        
+      </ul>
+      <!-- Left links -->
+    </div>
+    <!-- Collapsible wrapper -->
+  </div>
+  <!-- Container wrapper -->
+</nav>
